69ba85d3c578b6aa181e28294290db811986e461
Yuriy Kravets <zoidthe311@gmail.com>
2022-02-06 22:18:34 +0200

    basics of data input/output
    My studying

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+
+fun main () {
+    //ввод данных
+    val rectangle1: Int = 5
+    val rectangle: Int = 10
+
+    //обработка
+    val sum: Int = rectangle1 + rectangle
+
+    //вывод данных
+    println(sum)
+
+
+
+}
